164ba6b5641917bf75201764d21a573be983e5a0
youngchannoh <young-chan.noh@mitel.com>
2021-08-04 19:23:22 -0700

    update shell file to use workspace of jenkins because of permission issue.

--- drTests.sh
+++ drTests.sh
@@ -4,7 +4,8 @@ dockerImage=cribl_image
 environFile=env.list
 hostName=agent
 bridgNetworkName=cribl_network
-localDir=/home/shoretel/docker_containers/cribl
+#localDir=/home/shoretel/docker_containers/cribl
+localDir=/home/shoretel/jenkins_slave/workspace/ynoh-job-testCICD
 containerDir=/app
 fileToRunOnContainer=run_agent.sh
 ### -v :This is to sych local and container dir (Reason: RobotFramework to publish result to Jenkins) ###
